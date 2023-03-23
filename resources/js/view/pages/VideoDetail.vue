714f2589b8420b2c81a5b8a22ce1bb914d3723fa
NTruong <nhattruong.dev3@gmail.com>
2023-03-23 13:45:01 +0700

    update

--- resources/js/view/pages/VideoDetail.vue
+++ resources/js/view/pages/VideoDetail.vue
@@ -27,25 +27,7 @@
                     </div>
                     <p class="post-description">{{ Detail.description }}</p>
                 </div>
-                <div class="comments">
-                    <div class="content" id="content">
-                        <ul class="main-comment">
-                            <li>
-                                <Comment></Comment>
-                            </li>
-                        </ul>
-                    </div>
-                    <form class="post-comment" @submit.prevent="UpdateCmt">
-                        <input
-                            type="text"
-                            placeholder="Write some thing..."
-                            name="comment"
-                        />
-                        <button type="submit">
-                            <i class="fa-solid fa-paper-plane"></i>
-                        </button>
-                    </form>
-                </div>
+                <comment-layout @UpdateCmt="UpdateCmt"></comment-layout>
             </div>
         </div>
     </div>
@@ -174,13 +156,11 @@ hr {
 
 <script>
 import CommentLayout from "../components/CommentComponent/CommentLayout.vue";
-import Comment from "../components/CommentComponent/Comment.vue";
 import { VideoDetail } from "../../services/Watch";
 export default {
     setup() {},
     components: {
         CommentLayout,
-        Comment,
     },
     data() {
         return {
@@ -252,73 +232,3 @@ export default {
     },
 };
 </script>
-
-
-
-<style scoped>
-.comments {
-    background-color: #f8f8f8;
-    height: 100%;
-}
-.comments .content {
-    padding: 0px 20px 20px 20px;
-    height: 82%;
-    z-index: 0;
-}
-.main-comment {
-    max-height: 100%;
-    padding-top: 20px !important;
-}
-.sub-comment {
-    position: absolute;
-}
-ul {
-    padding: 0px !important;
-    margin: 0px !important;
-    overflow-y: auto;
-}
-.comments .content ul li {
-    list-style: none;
-}
-.post-comment {
-    display: flex;
-    align-items: center;
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    background-color: white;
-    height: 60px;
-    z-index: 100;
-}
-
-.post-comment input {
-    margin: 0px 20px;
-    width: 100%;
-    border: 0px;
-    outline: none;
-    border: 1px solid;
-    border-color: #e7edf277;
-    /* padding: 0px 25px 0px 2%; */
-    font-size: 13px;
-    height: 40px;
-    background-color: #f8f8f8;
-    padding: 0px 50px 0px 10px;
-}
-.post-comment .avt {
-    border-radius: 100%;
-    width: 45px;
-    height: 45px;
-}
-.post-comment button {
-    position: absolute;
-    right: 20px;
-    top: 8%;
-    background-color: transparent;
-    border: 0px;
-    transform: translate(-50%, 50%);
-}
-
-li:last-child {
-    padding-bottom: 50px;
-}
-</style>
