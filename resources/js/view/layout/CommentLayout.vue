c9be3fc7c75d351afb8a370e3ecaf957088b93d4
NTruong <nhattruong.dev3@gmail.com>
2023-03-28 14:49:30 +0700

    config_detail

--- resources/js/view/layout/CommentLayout.vue
+++ resources/js/view/layout/CommentLayout.vue
@@ -12,7 +12,11 @@
                 <li v-if="Comments.length != total_comment">
                     <div class="rep">
                         <span id="view-more">
-                            <div @click="ViewMoreComment">View more</div>
+                            <div @click="ViewMoreComment">
+                                {{ Comments.length }}View more{{
+                                    total_comment
+                                }}
+                            </div>
                         </span>
                     </div>
                 </li>
@@ -192,7 +196,7 @@ export default {
                 data.post_id = this.PostId;
                 this.UpdateCmt(data);
             }
-
+            this.total_comment += 1;
             e.target.comment.value = "";
         },
         UpdateCmt(data) {
